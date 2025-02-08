c439f4ba0c79d428cc80f9d5bc9446dabed6bd02
John Doe <rizharafif11@gmail.com>
2025-02-08 15:20:58 +0700

    add genre api

--- src/controllers/genreController.ts
+++ src/controllers/genreController.ts
@@ -1,20 +1,113 @@
 import type { Request, Response } from "express";
 import Genre from "../models/Genre";
+import { genreSchema } from "../utils/zodSchema";
 
 export const getGenres = async (req: Request, res: Response) => {
-    try {
-        const genres = await Genre.find()
+  try {
+    const genres = await Genre.find();
 
-        return res.json({
-            data: genres,
-            message: "Success get data",
-            status: "Success"
-        })
-    } catch (error) {
-        return res.status(500).json({
-            message: "Failed to get data",
-            data: null,
-            status: "Failed"
-        })
-    }
-}
+    return res.json({
+      data: genres,
+      message: "Success get data",
+      status: "Success",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Failed to get data",
+      data: null,
+      status: "Failed",
+    });
+  }
+};
+
+export const getGenreDetail = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const genre = await Genre.findById(id);
+
+    return res.json({
+      data: genre,
+      message: "Success get data",
+      status: "Success",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Failed to get data",
+      data: null,
+      status: "Failed",
+    });
+  }
+};
+
+export const postGenres = async (req: Request, res: Response) => {
+  try {
+    const body = genreSchema.parse(req.body);
+
+    const genre = new Genre({
+      name: body.name,
+    });
+
+    const newData = await genre.save();
+
+    return res.json({
+      message: "Success create data",
+      data: newData,
+      status: "Success",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Failed to create data",
+      data: null,
+      status: "Failed",
+    });
+  }
+};
+
+export const putGenres = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const body = genreSchema.parse(req.body);
+
+    await Genre.findByIdAndUpdate(id, {
+      name: body.name,
+    });
+
+    const updatedData = await Genre.findById(id);
+
+    return res.json({
+      message: "Success update data",
+      data: updatedData,
+      status: "Success",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Failed to update data",
+      data: null,
+      status: "Failed",
+    });
+  }
+};
+
+export const deleteGenres = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const deleteData = await Genre.findById(id);
+
+    await Genre.findByIdAndDelete(id);
+
+    return res.json({
+      message: "Success delete data",
+      data: deleteData,
+      status: "Success",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Failed to delete data",
+      data: null,
+      status: "Failed",
+    });
+  }
+};
--- src/routes/adminRoutes/genreRoutes.ts
+++ src/routes/adminRoutes/genreRoutes.ts
@@ -1,8 +1,14 @@
 import express from 'express'
-import { getGenres } from '../../controllers/genreController'
+import { deleteGenres, getGenres, getGenreDetail, postGenres, putGenres } from '../../controllers/genreController'
+import { validateRequest } from '../../middlewares/validateRequest'
+import { genreSchema } from '../../utils/zodSchema'
 
 const genreRoutes = express.Router()
 
 genreRoutes.get('/genres', getGenres)
+genreRoutes.get('/genres/:id', getGenreDetail)
+genreRoutes.post('/genres', validateRequest(genreSchema), postGenres)
+genreRoutes.put('/genres/:id', validateRequest(genreSchema), putGenres)
+genreRoutes.delete('/genres/:id', deleteGenres)
 
 export default genreRoutes

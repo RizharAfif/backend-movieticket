13c1a72d8cd02a63fc93d9cf63e1dadca4d62848
John Doe <rizharafif11@gmail.com>
2025-02-10 13:25:34 +0700

    integrate transaction api

--- src/routes/customerRoutes/globalRoutes.ts
+++ src/routes/customerRoutes/globalRoutes.ts
@@ -1,12 +1,28 @@
-import  express from 'express';
-import { getAvailablSeats, getGlobalGenre, getGlobalMovieDetail, getGlobalMovies, getMoviesFilter } from '../../controllers/globalController';
+import express from "express";
+import {
+  getAvailablSeats,
+  getGlobalGenre,
+  getGlobalMovieDetail,
+  getGlobalMovies,
+  getMoviesFilter,
+} from "../../controllers/globalController";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { transactionSchema } from "../../utils/zodSchema";
+import { getOrderDetails, getOrders, transactionTicket } from "../../controllers/ticketController";
 
 const globalRoutes = express.Router();
 
-globalRoutes.get('/movies', getGlobalMovies)
-globalRoutes.get('/genres', getGlobalGenre)
-globalRoutes.get('/movies/:id', getGlobalMovieDetail)
-globalRoutes.get('/check-seats/:movieId', getAvailablSeats)
-globalRoutes.get('/browse-movies/:genreId', getMoviesFilter)
+globalRoutes.get("/movies", getGlobalMovies);
+globalRoutes.get("/genres", getGlobalGenre);
+globalRoutes.get("/movies/:id", getGlobalMovieDetail);
+globalRoutes.get("/check-seats/:movieId", getAvailablSeats);
+globalRoutes.get("/browse-movies/:genreId", getMoviesFilter);
+globalRoutes.post(
+	"/transaction/buy",
+	validateRequest(transactionSchema),
+	transactionTicket,
+);
+globalRoutes.get("/orders", getOrders)
+globalRoutes.get("/orders/:id", getOrderDetails)
 
 export default globalRoutes;
--- src/utils/zodSchema.ts
+++ src/utils/zodSchema.ts
@@ -31,9 +31,20 @@ export const authSchema = z.object({
   name: z.string().min(5),
   email: z.string().email(),
   password: z.string().min(6),
-  role: z.enum(["admin", "customer"])
+  role: z.enum(["admin", "customer"]),
 });
 
 export const topupSchema = z.object({
   balance: z.number().min(1000),
-})
+});
+
+export const transactionSchema = z.object({
+  subtotal: z.number(),
+  total: z.number(),
+  bookingFee: z.number(),
+  tax: z.number(),
+  movieId: z.string(),
+  theaterId: z.string(),
+  seats: z.array(z.string()),
+  date: z.string(),
+}).strict();

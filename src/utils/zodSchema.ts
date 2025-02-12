1d37751cf7b5c792b36b6f9512c30b9620ad4306
John Doe <rizharafif11@gmail.com>
2025-02-12 15:30:35 +0700

    fix zod schema

--- src/utils/zodSchema.ts
+++ src/utils/zodSchema.ts
@@ -10,7 +10,7 @@ export const genreSchema = z
 
 export const theaterSchema = z
   .object({
-    name: z.string().min(5),
+    name: z.string().min(3),
     city: z.string().min(5),
   })
   .strict();
